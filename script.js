fe5f3e25ccab8887b6f83c9b92423844ba3823ed
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-06-03 08:53:59 -0700

    added questions

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log('working')
+//question database
+const STORE = [
+    {
+        question: 'What did Kevin buy for himself when he drew himself for secret santa?',
+        answers: [
+            'Foot massager',
+            'M&Ms',
+            'Foot bath',
+            'Toaster'
+        ],
+        correctAnswer:
+        'Foot massager',
+    },
+    {
+        question: 'During the episode, Prince Family Paper, what is the secret signal that Dwight and Michael agree on using?',
+        answers: [
+            'Raising your eyebrows',
+            'Licking your lips',
+            'Coughing loudly',
+            'Birdcall'
+        ],
+        correctAnswer:
+        'Raising your eyebrows'
+    },
+    {
+        question: 'What does Angela use as a rape deterrent instead of a whistle?',
+        answers: [
+            'Pepper spray',
+            'Rape flute',
+            'Blow horn',
+            'Yankee Doodle Dandy'
+        ],
+        correctAnswer:
+        'Yankee Doodle Dandy'
+    },
+    {
+        question: 'What does Michael eat instead of ice cream because they does not have any?',
+        answers: [
+            'Mayo and black olives',
+            'Mayo and chocolate chips',
+            'Mayo and a pickle',
+            'Mayo and M&Ms'
+        ],
+        correctAnswer:
+        'Mayo and black olives'
+    },
+    {
+        question: 'On what social media site does Creed say that Michaels nephew is trashing them relentlessly?',
+        answers: [
+            'Facebook',
+            'Twitter',
+            'Instagram',
+            'Woof'
+        ],
+        correctAnswer: 
+        'Facebook'
+    }
+];
+
+let score = 0;
+let questionNumber = 0;
+
+
+
